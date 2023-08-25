c0b74648700b0c8f539a05e3a1f932d720ed94df
AryanParmar007 <124689058+AryanParmar007@users.noreply.github.com>
2023-08-25 07:49:04 +0530

    Initial commit

---
+++ app/src/main/java/com/aryanparmar/mad_practical3_21012011061/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aryanparmar.mad_practical3_21012011061
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
